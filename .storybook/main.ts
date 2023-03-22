6daebe495ee64b367699bd67ac0fd2c83ce8a074
inovue <inovue3@gmail.com>
2023-03-22 18:57:22 +0900

    support storybook remarkgfm addons

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,12 +2,24 @@ import type { StorybookConfig } from "@storybook/react-vite";
 import { mergeConfig } from "vite";
 import {vanillaExtractPlugin} from "@vanilla-extract/vite-plugin";
 
+import remarkGfm from "remark-gfm";
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        mdxPluginOptions: {
+          mdxCompileOptions: {
+            remarkPlugins: [remarkGfm],
+          },
+        },
+      },
+    },
   ],
   framework: {
     name: "@storybook/react-vite",
